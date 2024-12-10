1e8a7947fd8be320f883c1b5ce5023aa1750bfe5
A1eksMa <a1ex_ma@mail.ru>
2024-12-10 15:11:56 +0300

    Add blank CustomLinkedListIteratorTest

---
+++ test/CustomLinkedListIteratorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomLinkedListIteratorTest {
+
+    @Test
+    public void testCustomLinkedListIterator() {
+        
+    }
+}
