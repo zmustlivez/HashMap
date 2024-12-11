695730d2542994a133bf19003977f11ec358892c
ml <mustlive@gmail.com>
2024-12-11 11:58:39 +0300

    Добавил метод remove, поправил 8 строку класса Node, чтобы можно было добавлять null

--- src/Node.java
+++ src/Node.java
@@ -5,7 +5,8 @@ public class Node<K,V> {
     private Node<K,V> next;
         
     public Node(K key, V value) {
-        this.hash = key.hashCode();
+//        this.hash = key.hashCode();
+        this.hash = (key == null) ? 0 : key.hashCode();
         this.key = key;
         this.value = value;
         this.next = null;
